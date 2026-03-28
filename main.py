0e5cab932b2bc603ada363906874d669f421a98e
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-03-28 09:06:22 -0500

    Add user input handling and output formatting
    This is still in basic more features comes up. I just dtected feeling and made them ready for spotify search.

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # Runs the program, takes user input and calls logic and finally prints the result
+from logic import process_input, format_output
+
+def ask_user():
+    # Get user input
+    feelings = input("How are you feeling today? > \n")
+    return feelings
+
+
+
+def main():
+    feelings = ask_user()
+    result = process_input(feelings)
+    text = format_output(result)
+    print(text)
+
+
+if __name__ == "__main__":
+    main()
